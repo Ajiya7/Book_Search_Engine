faba486c485a079ae6c5048e2fb6f0a8409806d9
Faruk Ajiya <Ajiyafaruk@gmail.com>
2023-03-08 15:31:47 -0800

    completed the backend specifications

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `#graphql
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book] 
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  input Input {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(newBook: Input ): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
